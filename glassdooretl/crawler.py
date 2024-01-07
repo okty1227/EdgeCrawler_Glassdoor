c9184bbd22f16f09827c382c3aecba41f321e32d
tl853 <tl853@cornell.edu>
2024-01-07 18:44:36 -0500

    cralwer_ver0.0

--- glassdooretl/crawler.py
+++ glassdooretl/crawler.py
@@ -4,8 +4,8 @@ from selenium.common.exceptions import NoSuchElementException
 # from bs4 import BeautifulSoup
 import time
 import json
-import asyncio
 import math
+import re
 
 class CONFIG():
     '''pending'''
@@ -38,40 +38,68 @@ class CONNECTION():
     ifEasyApply_Xpath = '/html/body/div[2]/div[1]/div[3]/div[2]/div[2]/div[1]/header/div[3]/div[2]'
     
     company_all_sect = 'section.JobDetails_jobDetailsSection__PJz1h'
-    # jd_selector = 'div > div:nth-child(1)'
-    jd_selector = 'div'
-    salary_selector = 'section:nth-child(2) > div > div > div:nth-child(1)'
-    size_selector = 'section:nth-child(3) > div > div > div:nth-child(1) > div'
-    found_year_selector = 'section:nth-child(3) > div > div > div:nth-child(2) > div'
-    industry_selector = 'section:nth-child(3) > div > div > div:nth-child(4) > div'
-    sector_selector = 'section:nth-child(3) > div > div > div:nth-child(5) > div'
-
-    def __init__(self,url:str,search_limit:int = 10 ,driver_appear:bool = False) -> None:
+    
+    jd_selector = ' > div'
+    salary_selector = ' > div > div > div:nth-child(1)'
+    
+    size_selector = ' > div > div > div:nth-child(1) > div'
+    found_year_selector = ' > div > div > div:nth-child(2) > div'
+    industry_selector = ' > div > div > div:nth-child(4) > div'
+    sector_selector = ' > div > div > div:nth-child(5) > div'
+    num_header_selector = '/html/body/div[2]/div[1]/div[3]/div[2]/div[1]/div[1]/h1'
+    click_dynamic_selector = 'arguments[0].click();'
+    def __init__(self,url:str,full_search=True,search_limit:int = 10 ,driver_appear:bool = False, show_web=False,end_with_web=False) -> None:
         self.url = url
-        self.search_limit = search_limit
-        options = webdriver.EdgeOptions()
-        # options.add_experimental_option("detach", True)
-        # driver = webdriver.Edge(options=options)
-        self.driver = webdriver.Edge()
-        self.driver.headless = driver_appear
-        self.search_limit = search_limit
-        self.stor_data = list()
-        self.ignored = 0 
-    def get_positio_amount(self):
         
-        amount_descript = self.driver.find_element('/html/body/div[2]/div[1]/div[3]/div[2]/div[1]/div[1]/h1').text
-        self.search_limit = int(amount_descript.split(' ')[0]) if int(amount_descript.split(' ')[0]) > self.search_limit else self.search_limit
+        options = webdriver.EdgeOptions()
+        
+        if end_with_web:                     
+            options.add_experimental_option("detach", True)
+        
+        if not show_web:
+            ## hide web browser
+            options.add_argument("--window-size=1920,1080")            
+            options.add_argument("--headless=new") 
+            options.add_argument("--disable-gpu")
+            options.add_argument("--no-sandbox")
+            user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+            options.add_argument(f'user-agent={user_agent}')
+
+        self.driver = webdriver.Edge(options=options)
+        self.driver.headless = driver_appear
+        self.search_limit = search_limit # search limit by each job key word
+        self.stor_data = list()
+        self.ignored = 0 # for test
+        self.if_full_search = full_search # collect every position for each job or not
+        
+        self.validate_salary = 'Average base salary estimate' # the string validating the section 
+        self.company_overview = 'Company overview'  # the string validating the section    
+         
+    def set_sec_validation_str(self,salary_str:str,company_str:str):
+        '''as cralwer locators are dynamics, the key word is set to make sure the
+            crawled section has the content we need'''
+        self.validate_salary = salary_str if salary_str else self.validate_salary
+        self.company_overview = company_str if company_str else self.company_overview
+
+    
+    def use_full_searched(self):
+        '''this function must be used before loading the crawler function'''
+        amount_descript = self.driver.find_element(By.XPATH,self.num_header_selector).text
+        
+        num_searched = int(re.search(r'(\d+)',re.sub(r',','',amount_descript)).group(1)) # the number of positions available
+        
+        self.search_limit = num_searched if num_searched > self.search_limit else self.search_limit
         
         return self.search_limit
     
     def connect_glassdoor(self): 
-
+        '''connect to job searched page'''
         self.driver.get(self.url)
     
-    def validate_element(self,parent_section_xpath,selector,defaule_val='-1'):
+    def validate_element(self,selector,defaule_val='-1'):
         try:
     
-            element = parent_section_xpath.find_element(By.CSS_SELECTOR,selector).get_attribute('innerText')      
+            element = self.driver.find_element(By.CSS_SELECTOR,self.company_all_sect + selector).text
             return element
         
         except NoSuchElementException as e:
@@ -79,7 +107,25 @@ class CONNECTION():
             print(f'{e}, default value -1 is given')
             return defaule_val
         
+    def validate_section_element(self,selector,val_str):
+        
+        validated_str_1 = self.driver.find_element(By.CSS_SELECTOR,self.company_all_sect + ' > section:nth-child(2)').text
+        validated_str_2 = self.driver.find_element(By.CSS_SELECTOR,self.company_all_sect + ' > section:nth-child(3)').text
+        val_bool = True
+        
+        if val_str in validated_str_1:
+            val_bool = False
+            return self.driver.find_element(By.CSS_SELECTOR,self.company_all_sect + ' > section:nth-child(2)'+selector).text
+        
+        if val_str in validated_str_2:
+            val_bool = False
+            return self.driver.find_element(By.CSS_SELECTOR,self.company_all_sect + ' > section:nth-child(3)'+selector).text
+        
+        if val_bool:
+            return 'none'
+        
     def retrieve_info(self):
+        
         
         stor_xpathtxt = {}
         trial = 0
@@ -87,39 +133,37 @@ class CONNECTION():
         while not self.driver.find_elements(By.CSS_SELECTOR,self.company_all_sect) and trial <5 :
             
             time.sleep(1)
-            trial += 1
-
+            trial += 1      
+            
         if trial <5:
-            ## only collect the company & position where location, position, rate, company name information are all provided
+            
+            ## only collect the company & position whose location, position, rate, company name information are all provided
             if len(self.driver.find_element(By.XPATH,self.basicInfo_Xpath).text.split('\n'))==4:
                 
                 stor_xpathtxt['position_info'] = self.driver.find_element(By.XPATH,self.basicInfo_Xpath).text
                 stor_xpathtxt['if_easy_apply'] = self.driver.find_element(By.XPATH,self.ifEasyApply_Xpath).text
-            
-                comp_all_element = self.driver.find_elements(By.CSS_SELECTOR, self.company_all_sect)
-                all_section = comp_all_element[0]
-                stor_xpathtxt['jd'] = self.validate_element(all_section,selector=self.jd_selector)
-                stor_xpathtxt['avg_salary'] = self.validate_element(all_section,selector=self.salary_selector)         
-                stor_xpathtxt['size'] = self.validate_element(all_section,selector=self.size_selector)       
-                stor_xpathtxt['found_year'] = self.validate_element(all_section,selector=self.found_year_selector)       
-                # stor_xpathtxt['industry'] = self.validate_element(all_section,selector=self.industry_selector)        
-                stor_xpathtxt['sector'] = self.validate_element(all_section,selector=self.sector_selector)   
-                      
+                            
+                stor_xpathtxt['jd'] = self.validate_element(selector=self.jd_selector)
+                stor_xpathtxt['avg_salary'] = self.validate_section_element(selector=self.salary_selector,val_str=self.validate_salary)         
+                stor_xpathtxt['size'] = self.validate_section_element(selector=self.size_selector,val_str=self.company_overview)       
+                stor_xpathtxt['found_year'] = self.validate_section_element(selector=self.found_year_selector,val_str=self.company_overview)       
+                stor_xpathtxt['sector'] = self.validate_section_element(selector=self.sector_selector,val_str=self.company_overview)                         
                 self.stor_data.append(stor_xpathtxt)
+                
             else:
-                self.ignored +=1
+                print(self.driver.find_element(By.XPATH,self.basicInfo_Xpath).text.split('\n')[1])
+                print('position is not collected because the basic information is not intact')
         else:
-            
+            self.driver.get_screenshot_as_file("./log/retrieve_info_err.png")
             pass
         
         
-    async def load_positions(self):
+    def load_positions(self):
         
-        for idx in range(0,int(self.search_limit)):   
-           # load each selection .click()      
-            
+        for idx in range(0,self.search_limit):   
+           
             position_sect = self.driver.find_element(By.XPATH, f'/html/body/div[2]/div[1]/div[3]/div[2]/div[1]/div[2]/ul/li[{idx+1}]')
-            self.driver.execute_script('arguments[0].click();',position_sect) 
+            self.driver.execute_script(self.click_dynamic_selector,position_sect) 
 
             # close all possible pop up windows
             if self.driver.find_elements(By.XPATH,self.exist_close_view):
@@ -132,24 +176,26 @@ class CONNECTION():
                 
             self.retrieve_info()
      
-    async def click_next_turn(self):
-        # CONNECTION.get_positio_amount()
-        for nb_turn in range(math.floor(self.search_limit/30)): #one fold explores 30 more position
+    def click_next_turn(self):
+        
+        for _ in range(math.floor(self.search_limit/30)): #one fold explores 30 more position
+            time.sleep(1)
+            self.driver.get_screenshot_as_file("./log/click_next_pg.png")
             next_locator = self.driver.find_element(By.XPATH,self.show_more_job)
-            
-            self.driver.execute_script('arguments[0].click();',next_locator)
+            self.driver.execute_script(self.click_dynamic_selector,next_locator)
+            time.sleep(3)
             print('successfully click next job page')
 
-    async def main(self):
-        self.connect_glassdoor()
-        
-        task1 = asyncio.create_task(self.click_next_turn())
-        task2 = asyncio.create_task(self.load_positions())
+    def main(self):
 
-        await task1
-        await task2
+        self.connect_glassdoor()
+        if self.if_full_search:
+            self.use_full_searched()
+            print('searched all position result')
+            
+        self.click_next_turn()
+        self.load_positions()
         print(self.ignored)
-        
         return self.stor_data
         
     def direct_to_search(self): ## currently replaced by url
--- glassdooretl/main.py
+++ glassdooretl/main.py
@@ -3,40 +3,35 @@
 # from selenium.common.exceptions import NoSuchElementException
 # from bs4 import BeautifulSoup
 # import time
-# import json
-import asyncio
+import json
 # import math
 from crawler import CONNECTION
 from preprocess import PREPROCESS
-from loadmongo import load_to_mongo_db
+# from loadmongo import load_to_mongo_db
 
-url_scientist = 'https://www.glassdoor.com/Job/united-states-data-scientist-jobs-SRCH_IL.0,13_IN1_KO14,28.htm'
-url_bi_analyst = 'https://www.glassdoor.com/Job/united-states-business-intelligence-analyst-jobs-SRCH_IL.0,13_IN1_KO14,43.htm'
-url_bi_engineer = 'https://www.glassdoor.com/Job/united-states-business-intelligence-engineer-jobs-SRCH_IL.0,13_IN1_KO14,44.htm'
-url_dataspecialist = 'https://www.glassdoor.com/Job/united-states-data-specialist-jobs-SRCH_IL.0,13_IN1_KO14,29.htm'
-url_data_engineer = 'https://www.glassdoor.com/Job/united-states-data-engineer-jobs-SRCH_IL.0,13_IN1_KO14,27.htm'
 
 port_ = "mongodb://localhost:8585/"
-# jobs_catalog = [url_scientist,
-#                      url_bi_analyst,
-#                      url_bi_engineer,
-#                      url_dataspecialist,
-#                      url_data_engineer]
-jobs_catalog = [url_bi_engineer,
-                     url_dataspecialist,
-                     url_data_engineer]
-async def run_main():
-    for job_link in jobs_catalog:
-        glassdoor_connect = CONNECTION(url=job_link)
-        result = await glassdoor_connect.main()
+jobs_catalog = {
+    'data_scientist':'https://www.glassdoor.com/Job/united-states-data-scientist-jobs-SRCH_IL.0,13_IN1_KO14,28.htm',
+    'bi_analyst': 'https://www.glassdoor.com/Job/united-states-business-intelligence-analyst-jobs-SRCH_IL.0,13_IN1_KO14,43.htm',
+    'bi_engineer': 'https://www.glassdoor.com/Job/united-states-business-intelligence-engineer-jobs-SRCH_IL.0,13_IN1_KO14,44.htm',
+    'dataspecialist':'https://www.glassdoor.com/Job/united-states-data-specialist-jobs-SRCH_IL.0,13_IN1_KO14,29.htm',
+    'data_engineer':'https://www.glassdoor.com/Job/united-states-data-engineer-jobs-SRCH_IL.0,13_IN1_KO14,27.htm'
+}
+
+def run_main():
+    for job_name, job_link in jobs_catalog.items():
+        glassdoor_connect = CONNECTION(url=job_link,full_search=False,search_limit=100)
+        result = glassdoor_connect.main()
         # print(result)
         processed = PREPROCESS.run_preprocess(crawed_raw_data=result)
-       
-        load_to_mongo_db(loaded_data=processed,port_link=port_)
+        with open(f'{job_name}_glassdoor.json','w') as file:
+            json.dump(obj=processed,fp=file) 
+        # load_to_mongo_db(loaded_data=processed,port_link=port_)
 
-asyncio.run(run_main())
 
-
+if __name__ == '__main__':
+    run_main()
 
 
 
